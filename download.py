dbd399c8afd1bdc42326129627748ef79d6ef11e
Mics <mics@mics.tw>
2023-10-05 15:04:36 +0800

    upd

--- download.py
+++ download.py
@@ -22,27 +22,30 @@ if not os.path.exists(temp_path):
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
-# TODO 檢查是否已重複下載
-
 # 下載影片
 video_file = stream.download(output_path=temp_path)
 
 # 取得影片檔案名稱 (不包含副檔名)
 video_name = os.path.splitext(os.path.basename(video_file))[0]
 
-# 分割人聲
-demucs.separate.main(shlex.split(f"--mp3 --two-stems vocals -n htdemucs_ft -d cuda \"{video_file}\" -o \"{temp_path}\""))
+# 人聲檔案名稱
+vocals_file = os.path.join(temp_path, f"htdemucs_ft", f"{video_name}", f"vocals.mp3")
+
+# 伴奏檔案名稱
+no_vocals_file = os.path.join(temp_path, f"htdemucs_ft", f"{video_name}", f"no_vocals.mp3")
+
+# 分割伴奏 (如果沒有)
+if not os.path.exists(no_vocals_file):
+    demucs.separate.main(shlex.split(f"--mp3 --two-stems vocals -n htdemucs_ft -d cuda \"{video_file}\" -o \"{temp_path}\""))
 
 # 使用 ffmpeg 合併左右聲道
 
-# 取得左聲道檔案名稱
-vocals_file = os.path.join(temp_path, f"htdemucs_ft", f"{video_name}", f"vocals.mp3")
-
-# 取得右聲道檔案名稱
-no_vocals_file = os.path.join(temp_path, f"htdemucs_ft", f"{video_name}", f"no_vocals.mp3")
-
 # 輸出
-output_file = os.path.join(output_path, f"{video_name}.mp4")
+output_ktv_file = os.path.join(output_path, f"{video_name}_KTV.mp4")
+output_no_vocals_file = os.path.join(output_path, f"{video_name}_NoVocals.mp4")
 
-# 執行 ffmpeg 將兩聲道與影片合併
-os.system(f"ffmpeg -i \"{video_file}\" -i \"{no_vocals_file}\" -filter_complex \"[0:a][1:a]amerge=inputs=2,pan=stereo|c0<c2+c3|c1<c0+c1\" -map 0:v -c:v copy \"{output_file}\"")
+# 左聲道用 no_vocals_file 右聲道用 video_file 影片用 video_file
+os.system(f"ffmpeg -y -i \"{video_file}\" -i \"{no_vocals_file}\" -filter_complex \"[0:a][1:a]amerge=inputs=2,pan=stereo|c0<c2+c3|c1<c0+c1\" -map 0:v -c:v copy \"{output_ktv_file}\"")
+
+# 聲音用 no_vocals_file 影片用 video_file
+os.system(f"ffmpeg -y -i \"{video_file}\" -i \"{no_vocals_file}\" -map 1:a -map 0:v -c:v copy \"{output_no_vocals_file}\"")
